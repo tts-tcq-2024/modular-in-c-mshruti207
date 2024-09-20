43a450beee469488190343e32f702447f770ef28
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-20 13:32:54 +0530

    Update ColorCodeTest.cpp

--- ColorCodeTest.cpp
+++ ColorCodeTest.cpp
@@ -1,1 +1,10 @@
-
+#include <gtest/gtest.h>
+#include "ColorCode.h"
+ 
+//Append zeros if result contains less than 3 digits
+TEST(SoudexTestsuite, PadZerosForSingleCharacter, wwd) {
+//AAA
+  
+ testPairToNumber(BLACK, ORANGE, 12);
+  testNumberToPair(4, WHITE, BROWN);
+}
