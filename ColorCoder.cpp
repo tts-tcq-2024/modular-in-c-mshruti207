581059726e47b2e232f43de231ccb3db0e1e7bf2
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-20 13:50:50 +0530

    Update ColorCoder.cpp

--- ColorCoder.cpp
+++ ColorCoder.cpp
@@ -31,20 +31,4 @@ int GetPairNumberFromColor(const ColorPair* colorPair) {
            colorPair->minorColor + 1;
 }
 
-void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) {
-    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
-    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
-    ColorPairToString(&colorPair, colorPairNames);
-    printf("Got pair %s\n", colorPairNames);
-  //  assert(colorPair.majorColor == expectedMajor);
-  //  assert(colorPair.minorColor == expectedMinor); // Uncommented assertion
-}
 
-void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) {
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-  //  printf("Got pair number %d\n", pairNumber);
-  //  assert(pairNumber == expectedPairNumber); // Uncommented assertion
-}
