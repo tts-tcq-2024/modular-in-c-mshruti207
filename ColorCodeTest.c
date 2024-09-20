a7254ed1237d652893a05ed5701a54a88ae1bc8e
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-20 14:09:49 +0530

    Update ColorCodeTest.c

--- ColorCodeTest.c
+++ ColorCodeTest.c
@@ -4,6 +4,30 @@
 #include <assert.h> // Include assert header
 #include <ColorCoder.h>
 
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(&colorPair, colorPairNames);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testPairToNumber(
+    enum MajorColor major,
+    enum MinorColor minor,
+    int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    printf("Got pair number %d\n", pairNumber);
+    assert(pairNumber == expectedPairNumber);
+}
 
 int main() {
     testNumberToPair(4, WHITE, BROWN);
