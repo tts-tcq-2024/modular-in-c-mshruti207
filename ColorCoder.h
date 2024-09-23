11de522bdba35a452e2f701782cdba126152b46f
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-09-23 10:51:02 +0530

    Update ColorCoder.h

--- ColorCoder.h
+++ ColorCoder.h
@@ -22,6 +22,11 @@ typedef struct {
     MinorColor minorColor;
 } ColorPair;
 
+extern const char* MajorColorNames[];
+extern const char* MinorColorNames[];
+extern const int numberOfMajorColors;
+extern const int numberOfMinorColors;
+
 ColorPair CreateColorPair(MajorColor major, MinorColor minor);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
